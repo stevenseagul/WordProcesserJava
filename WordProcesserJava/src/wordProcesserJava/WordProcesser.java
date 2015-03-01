6afaba455b5af94cecd0a8594841b50bf7126663
stevenseagul <jared.h.j@hotmail.com>
2015-02-28 20:29:17 -0800

    added gui, textarea to put words in

--- WordProcesserJava/src/wordProcesserJava/WordProcesser.java
+++ WordProcesserJava/src/wordProcesserJava/WordProcesser.java
@@ -1,9 +1,43 @@
 package wordProcesserJava;
 
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
 public class WordProcesser 
 {
+	static JTextArea mainTextArea = new JTextArea(40, 40);
+	
 	public static void main(String[] args)
 	{
-		System.out.println("Hello World!");
+		new WordProcesser();
+	}
+	
+	public WordProcesser()
+	{
+		//init main gui
+		JFrame mainGUI = new JFrame();
+		GridBagConstraints grid = new GridBagConstraints();
+		mainGUI.getContentPane().setLayout(new GridBagLayout());
+		mainGUI.setResizable(true);
+		mainGUI.setSize(500, 500);
+		
+		//textfields
+		mainTextArea.setLineWrap(true);
+		grid.gridx = 0;
+		grid.gridy = 0;
+		mainGUI.add(mainTextArea, grid);
+		
+		
+		//exit javaw.exe on close
+		mainGUI.addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent e)
+			{
+				System.exit(0);
+			}
+		});
+		
+		mainGUI.pack();
+		mainGUI.setVisible(true);
 	}
 }
